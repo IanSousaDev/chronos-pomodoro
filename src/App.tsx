03030ebc928e442b15425304dc2d655272c9b9fa
Ian Sousa <ianqwe285@gmail.com>
2025-09-25 15:50:00 -0300

    adicionando restante do layout da home page, mas sem estiliza-los

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
-
+import { CirclePlay } from 'lucide-react';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
-
   return (
     <>
       <Container>
@@ -22,6 +22,34 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='Tarefa'
+              id='meuInput'
+              type='text' 
+              required
+            />
+          </div>
+
+          <div className='formRow'>
+            <span>Lorem ipsum dolor sit amet consectetur.</span>
+          </div>
+
+          <div className='formRow'>
+            <span>ciclos</span>
+            <span>0 0 0 0 0</span>
+          </div>
+
+          <div className='formRow'>
+            <button>
+              <CirclePlay />
+            </button>
+          </div>
+        </form>
+      </Container>
     </>
-  )
+  );
 }
